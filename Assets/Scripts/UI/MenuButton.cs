a7a397db0912b6583834d37598592a31b0a2b29c
Epicdragonbro <david.rotmo@hotmail.no>
2022-01-26 15:08:16 +0100

    Beginning on menu design.
    Added and worked on a main menu and added fonts

---
+++ Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public GameObject[] UIToHide;
+    public GameObject[] UIToShow;
+
+    public int SceneToLoadInt = 0;
+    public string SceneToLoadStr;
+
+    public void ToggleUI()
+    {
+        foreach(GameObject ToShow in UIToShow)
+        {
+            ToShow.SetActive(true);
+        }
+
+        foreach(GameObject ToHide in UIToHide)
+        {
+            ToHide.SetActive(false);
+        }
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_STANDALONE
+            Application.Quit();
+        #endif
+        
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+
+    public void LoadScene()
+    {
+        if(SceneToLoadInt !=0 && SceneToLoadStr == null)
+        {
+            SceneManager.LoadScene(SceneToLoadInt);
+        } 
+        else if (SceneToLoadStr != null)
+        {
+            SceneManager.LoadScene(SceneToLoadStr);
+        }
+        else
+        {
+            Debug.LogError("Please designate a scene to load.");
+        }
+    }
+}
