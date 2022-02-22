372195944d530a883723220a5726321cb46da1d8
Henrik <theporkchopsxd@gmail.com>
2022-02-22 14:29:38 +0100

    Added VR without steamvr (fuck steamvr)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,68 +16,148 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private Vector3 direction;
     public bool stopMarker;
+    public bool UsingXR;
+
+    public float UnfuckValue;
+    private bool unfuckVrStart;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         rb = GetComponent<Rigidbody>();
+        (new System.Threading.Thread(() => {
+            System.Threading.Thread.Sleep(500);
+            unfuckVrStart = true;
+        })).Start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonUp(1))
+        if (unfuckVrStart)
         {
-            Vector3 averagePosition = Vector3.zero;
-            foreach (EntitySheep fish in GameObject.FindObjectsOfType<EntitySheep>())
-            {
-                averagePosition += fish.transform.position;
-            }
-            averagePosition /= GameObject.FindObjectsOfType<EntitySheep>().Length;
-            foreach (EntitySheep fish in GameObject.FindObjectsOfType<EntitySheep>())
-            {
-                fish.ForceGroup(averagePosition);
-            }
-            stopMarker = true;
+            transform.position = new Vector3(transform.position.x, UnfuckValue, transform.position.z);
+            unfuckVrStart = false;
         }
-        if (Input.GetMouseButton(1))
+        if (UsingXR)
         {
-            Marker.transform.position += transform.forward / 10;
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            Collider[] col = Physics.OverlapSphere(transform.position, ThreatenRange, (1 << LayerMask.NameToLayer("Bear")));
-
-            if (col.Length > 0)
+            if (Input.GetButton("LStickPush") && Input.GetButton("RStickPush"))
             {
-                foreach (Collider c in col)
+                Debug.Log("Quit");
+                #if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                    Application.Quit(0);
+                #endif
+            }
+            if (Input.GetButtonDown("X"))
+            {
+                if (Physics.Raycast(transform.position + new Vector3(0.0f, 0.5f, 0.0f), transform.forward, out RaycastHit hit, InteractRange, 1 << LayerMask.NameToLayer("Artifact") | 1 << LayerMask.NameToLayer("NPC")))
                 {
-                    c.GetComponentInParent<EntityBear>().Scare(ThreatenAmount);
+                    if (hit.transform.TryGetComponent<IPickup>(out IPickup pickup))
+                    {
+                        GameManager.Instance.SplashText("You picked up " + pickup.PickupName);
+                        GameManager.Instance.artifactGET = true;
+                        Destroy(hit.transform.gameObject);
+                    }
+                    if (hit.transform.tag == "Quest")
+                    {
+                        hit.transform.GetComponent<MissionGiver>().StartMission();
+                    }
+                }
+            }
+            Vector2 joyAxis = new Vector2(Input.GetAxisRaw("RHorizontal"), Input.GetAxisRaw("RVertical"));
+
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, joyAxis.x, 0));
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-joyAxis.y, 0, 0));
+
+            if (Input.GetButtonDown("A"))
+            {
+                Vector3 averagePosition = Vector3.zero;
+                foreach (EntitySheep fish in GameObject.FindObjectsOfType<EntitySheep>())
+                {
+                    averagePosition += fish.transform.position;
+                }
+                averagePosition /= GameObject.FindObjectsOfType<EntitySheep>().Length;
+                foreach (EntitySheep fish in GameObject.FindObjectsOfType<EntitySheep>())
+                {
+                    fish.ForceGroup(averagePosition);
+                }
+            }
+            if (Input.GetButtonDown("B"))
+            {
+                Collider[] col = Physics.OverlapSphere(transform.position, ThreatenRange, (1 << LayerMask.NameToLayer("Bear")));
+
+                if (col.Length > 0)
+                {
+                    foreach (Collider c in col)
+                    {
+                        c.GetComponentInParent<EntityBear>().Scare(ThreatenAmount);
+                    }
                 }
             }
         }
-        if (Input.GetKeyDown(KeyCode.E))
+        else
         {
-            if (Physics.Raycast(transform.position + new Vector3(0.0f, 0.5f, 0.0f), transform.forward, out RaycastHit hit, InteractRange, 1 << LayerMask.NameToLayer("Artifact") | 1 << LayerMask.NameToLayer("NPC")))
+            if (Input.GetKeyDown(KeyCode.E))
             {
-                if (hit.transform.TryGetComponent<IPickup>(out IPickup pickup))
+                if (Physics.Raycast(transform.position + new Vector3(0.0f, 0.5f, 0.0f), transform.forward, out RaycastHit hit, InteractRange, 1 << LayerMask.NameToLayer("Artifact") | 1 << LayerMask.NameToLayer("NPC")))
                 {
-                    GameManager.Instance.SplashText("You picked up " + pickup.PickupName);
-                    GameManager.Instance.artifactGET = true;
-                    Destroy(hit.transform.gameObject);
-                }
-                if (hit.transform.tag == "Quest")
-                {
-                    hit.transform.GetComponent<MissionGiver>().StartMission();
+                    if (hit.transform.TryGetComponent<IPickup>(out IPickup pickup))
+                    {
+                        GameManager.Instance.SplashText("You picked up " + pickup.PickupName);
+                        GameManager.Instance.artifactGET = true;
+                        Destroy(hit.transform.gameObject);
+                    }
+                    if (hit.transform.tag == "Quest")
+                    {
+                        hit.transform.GetComponent<MissionGiver>().StartMission();
+                    }
                 }
             }
+            if (Input.GetMouseButtonDown(1))
+            {
+                Vector3 averagePosition = Vector3.zero;
+                foreach (EntitySheep fish in GameObject.FindObjectsOfType<EntitySheep>())
+                {
+                    averagePosition += fish.transform.position;
+                }
+                averagePosition /= GameObject.FindObjectsOfType<EntitySheep>().Length;
+                foreach (EntitySheep fish in GameObject.FindObjectsOfType<EntitySheep>())
+                {
+                    fish.ForceGroup(averagePosition);
+                }
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                Collider[] col = Physics.OverlapSphere(transform.position, ThreatenRange, (1 << LayerMask.NameToLayer("Bear")));
+
+                if (col.Length > 0)
+                {
+                    foreach (Collider c in col)
+                    {
+                        c.GetComponentInParent<EntityBear>().Scare(ThreatenAmount);
+                    }
+                }
+            }
+
+            Vector2 mouseAxis = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, mouseAxis.x, 0));
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-mouseAxis.y, 0, 0));
         }
-        Vector2 mouseAxis = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, mouseAxis.x, 0));
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(-mouseAxis.y, 0, 0));
+        float updown; 
+        if (UsingXR)
+        {
+            updown = Input.GetAxisRaw("RShoulder") - Input.GetAxisRaw("LShoulder");
+        }
+        else
+        {
+            updown = ((Input.GetKey(KeyCode.Space) ? 1 : 0) + (Input.GetKey(KeyCode.LeftShift) ? -1 : 0));
+        }
 
-        direction = (transform.right * Input.GetAxisRaw("Horizontal") + transform.up * ((Input.GetKey(KeyCode.Space) ? 1 : 0) + (Input.GetKey(KeyCode.LeftShift) ? -1 : 0)) + transform.forward * Input.GetAxisRaw("Vertical")).normalized;
+        direction = (transform.right * Input.GetAxisRaw("LHorizontal") + transform.up * updown + transform.forward * Input.GetAxisRaw("LVertical")).normalized;
     }
 
     void FixedUpdate()
